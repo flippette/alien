ef02d6e0cb7e85191aa45a57c4b99a4b30b35d31
flippette <me@flippo.cc>
2024-11-24 02:00:08 +0200

    Plumb item pickup and drop into Game

--- Alien/alien/Game.scala
+++ Alien/alien/Game.scala
@@ -20,4 +20,13 @@ class Game:
   def isWon: Boolean =
     this.player.health > 0 && this.playerRoom.name == "escape pod"
   def isLost: Boolean = this.player.health == 0
+
+  // Have the player pick up an item from the room they're in, returning whether
+  // the item was picked up.
+  def pickUp(idx: Int): Boolean =
+    this.playerRoom.removeItem(idx).map(this.player.pickUpItem(_)).isDefined
+  // Have the player drop an item into the room they're in, returning whether
+  // the item was dropped.
+  def drop(idx: Int): Boolean =
+    this.player.dropItem(idx).map(this.playerRoom.addItem(_)).isDefined
 end Game
